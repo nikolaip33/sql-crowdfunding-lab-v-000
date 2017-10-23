5a2928e91ef74b0e7fc14692cf0622cc36f0c189
Nikolai Pawlowski <nikolaip@gmail.com>
2017-10-23 12:52:18 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -20,7 +20,7 @@ GROUP BY users.name ORDER BY users.name ASC;"
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-"SELECT projects.title, projects.funding_goal - SUM(pledges.amount) FROM projects
+"SELECT projects.title FROM projects
 JOIN pledges ON projects.id = pledges.project_id
 WHERE SUM(pledges.amount) > projects.funding_goal GROUP BY projects.id"
 end
