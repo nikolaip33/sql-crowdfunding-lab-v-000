61bd2e45437d9f35ec88f34a531fd3b164d218ed
Nikolai Pawlowski <nikolaip@gmail.com>
2017-10-23 11:53:46 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -13,7 +13,7 @@ ORDER BY projects.title ASC;"
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
-"SELECT users.name, users.age, pledges.amount FROM projects
+"SELECT users.name, users.age, SUM(pledges.amount) FROM projects
 JOIN pledges ON projects.id = pledges.project_id
 JOIN users ON pledges.user_id = users.id
 ORDER BY users.name ASC;"
