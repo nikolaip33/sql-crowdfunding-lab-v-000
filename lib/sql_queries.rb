8216c259dd80b026d1da6997154b954445948de4
Nikolai Pawlowski <nikolaip@gmail.com>
2017-10-23 11:50:32 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -13,7 +13,8 @@ ORDER BY projects.title ASC"
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
-"Write your SQL query Here"
+"SELECT FROM projects JOIN pledges ON projects.id = pledges.project_id
+JOIN users ON "
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
